e4921f6427c4296449a6117753bb52cdd63de882
Y11o <Y11o@users.noreply.github.com>
2024-11-18 00:42:05 +0300

    Init commit

---
+++ src/main/java/ru/spb/vika/VikaApplication.java
@@ -0,0 +1,13 @@
+package ru.spb.vika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spb/vika/vika/VikaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spb.vika.vika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
