0c992995dc402de5a65dc147256cbc74ab366afc
Y11o <Y11o@users.noreply.github.com>
2024-11-18 00:58:07 +0300

    Model mapper added

--- src/main/java/ru/spb/vika/VikaApplication.java
+++ src/main/java/ru/spb/vika/VikaApplication.java
@@ -1,7 +1,9 @@
 package ru.spb.vika;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VikaApplication {
@@ -10,4 +12,8 @@ public class VikaApplication {
 		SpringApplication.run(VikaApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
